36d6bbaed75e40224d18a4d7507cb59c17b53e91
stanislavbublinsky <bublinsky.s.m@gmail.com>
2022-11-10 13:11:59 +0200

    created template folder with index.html file; adde intex url & view

---
+++ manager/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from manager.views import (
+    index,
+)
+
+urlpatterns = [
+    path("", index, name="index"),
+
+]
+
+app_name = "manager"
--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,20 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from manager.models import Task, TaskType
+
+
+@login_required
+def index(request):
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "task_count": Task.objects.count(),
+        "task_type_count": TaskType.objects.count(),
+        "worker_count": get_user_model().objects.count(),
+        "num_visits": num_visits
+    }
+
+    return render(request, "manager/index.html", context=context)
