b7c10761dd6cad8297428c87a100f9c2d7f16edd
stanislavbublinsky <bublinsky.s.m@gmail.com>
2022-11-14 10:02:07 +0200

    hided secret key

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-j3rr35n%u1^0vckif$o*fnev73i88e0)ra3bfsu75$9k^a_xnt")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
